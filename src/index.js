36c237c7fce5c94785df7ee23b7cddfee65ab937
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2025-01-29 21:14:28 +0200

    add and fix script

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ if (module.hot) {
 import "./sass/main.scss";
 import "./js/main";
 import "./js/btn";
-import "./js/submit-form"
+// import "./js/submit-form"
 // import "./js/service-worker-start"
 import headerHTML from "./partials/header.html";
 import heroHTML from "./partials/hero.html";
@@ -23,7 +23,7 @@ document.getElementById("hero").innerHTML = heroHTML;
 document.getElementById("whatWeDo").innerHTML = whatWeDoHTML;
 document.getElementById("ourServices").innerHTML = ourServicesHTML;
 document.getElementById("ourPortfolio").innerHTML = ourPortfolioHTML;
-// document.getElementById("pricing").innerHTML = pricingHTML;
+document.getElementById("pricing").innerHTML = pricingHTML;
 document.getElementById("contactUs").innerHTML = contactUsHTML;
 document.getElementById("footer").innerHTML = footerHTML;
 
--- src/js/submit-form.js
+++ src/js/submit-form.js
@@ -1,25 +1,25 @@
+console.log("submit foo loaded")
 async function submitForm(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const formData = Object.fromEntries(new FormData(event.target).entries());
+  const formData = Object.fromEntries(new FormData(event.target).entries());
 
-    try {
-      const response = await fetch(
-        "https://www.form-to-email.com/api/s/UTiw851OywNk",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(formData),
-        }
-      );
-      if (!response.ok) {
-        // in case of malformed form data
-      } else {
-        // in case of success
-      }
-    } catch (error) {
-      // in case of form-to-email server not responding
+  try {
+    // const response = await fetch("https://www.form-to-email.com/api/s/UTiw851OywNk", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(formData),
+    // });
+    console.log("sending");
+    alert("sending...");
+    if (response.ok) {
+      // in case of success
+    } else {
+      // in case of malformed form data
     }
+  } catch (error) {
+    // in case of form-to-email server not responding (e.g. connection lost)
   }
+}
