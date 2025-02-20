59eaa39cd7db51af70c215edf8dd9744a2d672b4
GallaDindorogo <107935917+GallaDindorogo@users.noreply.github.com>
2025-02-20 22:36:52 +0100

    add preloader
    two options
    1 - just the name WeebBees
    2 - logo-svg
    can also add animation

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import "./js/pricing";
 
 // import "./js/submit-form"
 // import "./js/service-worker-start"
+import preloaderHTML from "./partials/preloader.html";
 import headerHTML from "./partials/header.html";
 import heroHTML from "./partials/hero.html";
 import whatWeDoHTML from "./partials/whatWeDo.html";
@@ -22,6 +23,7 @@ import pricingHTML from "./partials/pricing.html";
 import contactUsHTML from "./partials/contactUs.html";
 import footerHTML from "./partials/footer.html";
 
+document.getElementById("preloader").innerHTML = preloaderHTML;
 document.getElementById("header").innerHTML = headerHTML;
 document.getElementById("hero").innerHTML = heroHTML;
 document.getElementById("whatWeDo").innerHTML = whatWeDoHTML;
